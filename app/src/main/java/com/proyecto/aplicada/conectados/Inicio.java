2fa9fdb5a3ea9c141f4f6d3a03a82e8ac7a68f7e
Luis González <luis0270@gmail.com>
2016-11-16 17:29:11 -0600

    Cambios minimos

---
+++ app/src/main/java/com/proyecto/aplicada/conectados/Inicio.java
@@ -0,0 +1,43 @@
+package com.proyecto.aplicada.conectados;
+
+
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Inicio extends AppCompatActivity implements View.OnClickListener{
+
+    TextView recuperarPw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        recuperarPw = (TextView)findViewById(R.id.idviewContraseñaOlv);
+        recuperarPw.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.idviewContraseñaOlv:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Se procederá a verificar su identidad")
+                        .setTitle("Recuperando contraseña")
+                        .setCancelable(false)
+                        .setNeutralButton("Aceptar",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                break;
+        }
+
+    }
+}
